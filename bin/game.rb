30f2265b8852d8555285ac0b46fb032e1779b74f
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-28 11:53:37 -0400

    Add User Stories
    - Create Person
    - Create Player

---
+++ bin/game.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+require_relative '../lib/person'
+require_relative '../lib/player'
+
+puts "Starting the game..."
+
+jack = Person.new('Jack', 'Sparrow')
+
+binding.pry
+
+puts "Created #{jack.full_name}"
+
+game_man = Player.new("Kathleen", "V")
+puts "game man's health is #{game_man.health}"
+
+
---
+++ lib/player.rb
@@ -0,0 +1,24 @@
+require_relative 'person'
+
+class Player < Person
+  # Create class constants
+   DEFAULT_HEALTH = 20
+   DEFAULT_STRENGTH = 5
+
+   attr_reader :health, :strength
+
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @health = DEFAULT_HEALTH
+    @strength = DEFAULT_STRENGTH
+  end
+
+  def alive?
+    health > 0
+  end
+
+  def take_damage(attack_strength)
+    @health -= attack_strength
+  end
+
+end
