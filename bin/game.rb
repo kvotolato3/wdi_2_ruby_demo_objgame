19d5cf60e9e4a9518ff2680ef39d8a9ac3489050
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-28 12:02:22 -0400

    Add take_damage to game.rb

--- bin/game.rb
+++ bin/game.rb
@@ -7,13 +7,18 @@ puts "Starting the game..."
 
 jack = Person.new('Jack', 'Sparrow')
 
-binding.pry
+
 
 puts "Created #{jack.full_name}"
 
 game_man = Player.new("Kathleen", "V")
 puts "game man's health is #{game_man.health}"
 
+puts "Game man is " + (game_man.alive? ? "alive" : "dead")
+
+game_man.take_damage(30)
+
+puts "Game man health is #{game_man.health} "
 puts "Game man is " + (game_man.alive? ? "alive" : "dead")
 
 
